77b863fe9cbb57759b0bff5b4bbc18eeadd02b1c
RomSribn <steeper.fest@gmail.com>
2018-10-18 19:41:42 +0300

    CounterFinal

--- Module8/Tasks1.js
+++ Module8/Tasks1.js
@@ -44,30 +44,69 @@ const log = txt => console.log(txt);
 */
 
 // const btn = document.querySelectorAll('button.btn');
-const val = document.querySelector('span.js-value');
-log(val)
-let res = 0;
+// const val = document.querySelector('span.js-value');
+// // log(typeof +val.textContent)
+// let res = 0;
 
-const plusAcc = document.querySelector('button.js-sup');
-plusAcc.addEventListener('click', handlePlus);
+// const plusAcc = document.querySelector('button.js-add');
+// // log(plusAcc)
+// plusAcc.addEventListener('click', handlePlus);
 
-const minusAcc = document.querySelector('button.js-sub');
-minusAcc.addEventListener('click', handleMinus);
+// const minusAcc = document.querySelector('button.js-sub');
+// // log(minusAcc)
+// minusAcc.addEventListener('click', handleMinus);
 
 
-function handlePlus(){
-  res += 1;
-  return val.textContent = res;
+// function handlePlus(){
+//   res += 1;
+//   return val.textContent = res;
+// }
+
+// function handleMinus(){
+// 	if(+val.textContent <= 0) return
+//   res -= 1;
+//   return val.textContent = res;
+// }
+//==================================
+/*
+  Есть форма с набором радиокнопок. Пользователь выбирает вариант ответа, 
+  после чего нажимает кнопку "Send" и происходит отправка формы.
+  
+  При отправке формы:
+    - не должна перезагружаться страница
+    - необходимо получить выбранную опцию и вывести в абзац с классом .result
+*/
+
+// const impContent = document.querySelectorAll('div.options>label>input[type="radio"]');
+// // impContent.setAttribute('some', 4)
+// // log(impContent)
+// const btn = document.querySelector('button.btn');
+// // log(btn);
+// const resultPlace = document.querySelector('p.result');
+// // log(resultPlace);
+// const form = document.querySelector('.question-form');
+// // form.addEventListener('submit', handleGetOptions);
+// impContent.addEventListener('click', handleGetOptions)
+// function handleGetOptions(event){
+// 	event.preventDefault()
+// 	const target = event.target;
+//   if(impContent.getAttributes('checked')){
+//   	return resultPlace.textContent = `Result: ${impContent.getAttributes('value')}`
+//   }
+// }
+
+let form = document.querySelector('form'),
+	submit = document.querySelector('button'),
+	result = document.querySelector('.result'),
+	val = null
+form.onclick = function(e){
+	let target = e.target;
+	if(target.tagName == 'INPUT') val =  target.value;
 }
 
-function handleMinus(){
-  res -= 1;
-  return val.textContent = res;
+submit.onclick = function(){
+result.innerHTML = val
 }
-
-
-
-
 
 
 
